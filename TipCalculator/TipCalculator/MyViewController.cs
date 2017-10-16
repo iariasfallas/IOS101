31d96deed1eb734e363d5ba4f12218173b007912
Israel Arias Fallas <israel.arias@grupobabel.com>
2017-10-15 19:29:32 -0600

    IOS101 - Exercise #3

--- TipCalculator/TipCalculator/MyViewController.cs
+++ TipCalculator/TipCalculator/MyViewController.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Foundation;
 using UIKit;
+using CoreGraphics;
 
 namespace TipCalculator
 {
@@ -13,6 +14,47 @@ namespace TipCalculator
         public MyViewController()
         {
             
+        }
+
+        /// <summary>
+        /// Override the method.
+        /// Add the controls
+        /// </summary>
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // or this.
+            View.BackgroundColor = UIColor.LightGray;
+
+            UITextField totalAmount = new UITextField()
+            {
+                Frame = new CGRect(20, 28, View.Bounds.Width - 40, 35),
+                KeyboardType = UIKeyboardType.DecimalPad,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter Total Amount"
+            };
+
+            UIButton calcButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CGRect(
+                    20, 71, View.Bounds.Width - 40, 45),
+                BackgroundColor = UIColor.FromRGB(0f, 0.5f, 0f)
+            };
+
+            calcButton.SetTitle("Calculate", UIControlState.Normal);
+
+            UILabel resultLabel = new UILabel()
+            {
+                Frame = new CGRect(
+                    x: 20, y: 124, width: View.Bounds.Width - 40, height: 40),
+                TextColor = UIColor.Blue,
+                TextAlignment = UITextAlignment.Center,
+                Font = UIFont.SystemFontOfSize(24),
+                Text = "Tip is $0.00"
+            };
+
+            View.AddSubviews(totalAmount, calcButton, resultLabel);
+
         }
     }
 }
